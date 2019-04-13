2984cfb5d1acc529d3c9f43357d3d1a2deac1cdf
Chanan Sukjirawanit <fort2002@hotmail.com>
2019-04-13 18:52:14 +0700

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Site made with Mobirise Website Builder v4.8.10, https://mobirise.com -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="generator" content="Mobirise v4.8.10, mobirise.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <link rel="shortcut icon" href="assets/images/logo4.png" type="image/x-icon">
+    <meta name="description" content="">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+    <link rel="stylesheet" href="assets/tether/tether.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="assets/theme/css/style.css">
+    <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/navbar.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/app.css" type="text/css">
+</head>
+
+<body>
+    <nav class="navbar navbar-toggleable-md navbar-dark bg-faded fixed-top">
+        <a class="navbar-brand mx-auto" href="#">
+            <div id="logo">
+                <img src="assets/images/chocofruit.png" style="width: 100%;">
+            </div>
+        </a>
+    </nav>
+    <section class="carousel slide cid-rmW1bhsrxd" data-interval="false" id="slider1-0">
+        <div class="full-screen">
+            <div class="mbr-slider slide carousel" data-pause="true" data-keyboard="false" data-ride="carousel" data-interval="4000">
+                <ol class="carousel-indicators">
+                    <li data-app-prevent-settings="" data-target="#slider1-0" data-slide-to="0"></li>
+                    <li data-app-prevent-settings="" data-target="#slider1-0" data-slide-to="1"></li>
+                    <li data-app-prevent-settings="" data-target="#slider1-0" class=" active" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item slider-fullscreen-image active" data-bg-video-slide="false" style="background-image: url(assets/images/mbr-1920x1275.jpg);">
+                        <div class="container container-slide">
+                            <div class="image_wrapper">
+                                <div class="mbr-overlay"></div><img src="assets/images/mbr-1920x1275.jpg">
+                                <div class="carousel-caption justify-content-center">
+                                    <div class="col-10 align-center">
+                                        <h2 class="mbr-fonts-style display-1" style="font-family: 'Kanit', sans-serif;">รับทำเว็บไซต์</h2>
+                                        <p class="lead mbr-text mbr-fonts-style display-5" style="font-family: 'Kanit', sans-serif;">
+                                            บริการรับทำเว็บไซต์ครบวงจรทุกรูปแบบ
+                                        </p>
+                                        <div class="mbr-section-btn" buttons="0">
+                                          <a class="btn btn-success display-4" href="/main">
+                                            <i class="fa fa-fw fa-door-open"></i>&nbsp;ENTER SITE
+                                          </a> 
+                                          <a class="btn  btn-white-outline display-4" href="/contact">
+                                            <i class="fa fa-fw fa-phone-square "></i>&nbsp;CONTACT US
+                                          </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item slider-fullscreen-image" data-bg-video-slide="false" style="background-image: url(assets/images/mbr-1920x1080.jpg);">
+                        <div class="container container-slide">
+                            <div class="image_wrapper">
+                                <div class="mbr-overlay"></div><img src="assets/images/mbr-1920x1080.jpg">
+                                <div class="carousel-caption justify-content-center">
+                                    <div class="col-10 align-center">
+                                        <h2 class="mbr-fonts-style display-1" style="font-family: 'Kanit', sans-serif;">ตกแต่งเว็บไซต์</h2>
+                                        <p class="lead mbr-text mbr-fonts-style display-5" style="font-family: 'Kanit', sans-serif;">
+                                            บริการแก้ไข ตกแต่งเว็บไซต์ทุกประเภท
+                                        </p>
+                                        <div class="mbr-section-btn" buttons="0">
+                                          <a class="btn btn-success display-4" href="/main">
+                                            <i class="fa fa-fw fa-door-open"></i>&nbsp;ENTER SITE
+                                          </a> 
+                                          <a class="btn  btn-white-outline display-4" href="/contact">
+                                            <i class="fa fa-fw fa-phone-square "></i>&nbsp;CONTACT US
+                                          </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item slider-fullscreen-image" data-bg-video-slide="false" style="background-image: url(assets/images/mbr-1-1920x1280.jpg);">
+                        <div class="container container-slide">
+                            <div class="image_wrapper">
+                                <div class="mbr-overlay"></div><img src="assets/images/mbr-1-1920x1280.jpg">
+                                <div class="carousel-caption justify-content-center">
+                                    <div class="col-10 align-center">
+                                        <h2 class="mbr-fonts-style display-1" style="font-family: 'Kanit', sans-serif;">ออกแบบระบบ</h2>
+                                        <p class="lead mbr-text mbr-fonts-style display-5" style="font-family: 'Kanit', sans-serif;">
+                                            บริการออกแบบ Software และ ระบบต่างๆ
+                                        </p>
+                                        <div class="mbr-section-btn" buttons="0">
+                                          <a class="btn btn-success display-4" href="/main">
+                                            <i class="fa fa-fw fa-door-open"></i>&nbsp;ENTER SITE
+                                          </a> 
+                                          <a class="btn  btn-white-outline display-4" href="/contact">
+                                            <i class="fa fa-fw fa-phone-square "></i>&nbsp;CONTACT US
+                                          </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div><a data-app-prevent-settings="" class="carousel-control carousel-control-prev" role="button" data-slide="prev" href="#slider1-0"><span aria-hidden="true" class="mbri-left mbr-iconfont"></span><span class="sr-only">Previous</span></a><a data-app-prevent-settings="" class="carousel-control carousel-control-next" role="button" data-slide="next" href="#slider1-0"><span aria-hidden="true" class="mbri-right mbr-iconfont"></span><span class="sr-only">Next</span></a>
+            </div>
+        </div>
+    </section>
+    <section class="engine"><a href="https://mobirise.info/o">free portfolio web templates</a></section>
+    <script src="assets/web/assets/jquery/jquery.min.js"></script>
+    <script src="assets/popper/popper.min.js"></script>
+    <script src="assets/tether/tether.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/ytplayer/jquery.mb.ytplayer.min.js"></script>
+    <script src="assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
+    <script src="assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
+    <script src="assets/smoothscroll/smooth-scroll.js"></script>
+    <script src="assets/theme/js/script.js"></script>
+    <script src="assets/slidervideo/script.js"></script>
+    <script src="assets/theme/js/fontawesome.min.js"></script>
+</body>
+
+</html>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Site made with Mobirise Website Builder v4.8.10, https://mobirise.com -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="generator" content="Mobirise v4.8.10, mobirise.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <link rel="shortcut icon" href="assets/images/logo4.png" type="image/x-icon">
+    <meta name="description" content="">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/tether/tether.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="assets/theme/css/sidenav.css">
+    <link rel="stylesheet" type="text/css" href="assets/theme/css/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-md fixed-top font-weight-bold">
+        <a class="navbar-brand p-2" href="#">
+           <img src="assets/images/logo150.png" style="width: 100%;">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent"
+                aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        &nbsp;<i class="fas fa-home"></i>&nbsp; หน้าหลัก
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        &nbsp;<i class="fas fa-info-circle"></i>&nbsp; เกี่ยวกับ
+                    </a>
+                </li>
+               
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        &nbsp;<i class="fab fa-github"></i>&nbsp; Github
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        &nbsp;<i class="fas fa-sign-in-alt"></i>&nbsp; Login
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <nav class="sidenav">
+    </nav>
+    <section class="header1 cid-rnvY8t26A7 mbr-parallax-background" id="header1-4">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.7; background-color: rgb(0, 0, 0);">
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title align-center mbr-bold pb-3 mbr-fonts-style display-1">
+                    FULL WIDTH INTRO
+                </h1>
+                <h3 class="mbr-section-subtitle align-center mbr-light pb-3 mbr-fonts-style display-2">
+                    Beautiful mobile websites
+                </h3>
+                <p class="mbr-text align-center pb-3 mbr-fonts-style display-5">
+                    Full width intro with adjustable height, background image and a color overlay. Click any text to edit or style it.
+                </p>
+                <div class="mbr-section-btn align-center">
+                    <a class="btn btn-md btn-primary display-4" href="https://mobirise.co">
+                        <span class="mbr-iconfont mbri-file"></span>LEARN MORE</a>
+                    <a class="btn btn-md btn-white-outline display-4" href="https://mobirise.co">
+                        <span class="mbr-iconfont mbri-idea"></span>LIVE DEMO</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+	</section>
+    <script src="assets/web/assets/jquery/jquery.min.js"></script>
+    <script src="assets/popper/popper.min.js"></script>
+    <script src="assets/tether/tether.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/ytplayer/jquery.mb.ytplayer.min.js"></script>
+    <script src="assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
+    <script src="assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
+    <script src="assets/smoothscroll/smooth-scroll.js"></script>
+    <script src="assets/slidervideo/script.js"></script>
+    <script src="assets/theme/js/fontawesome.min.js"></script>
+	<script src="assets/theme/js/sidenav.js"></script>
+</body>
+
+</html>
